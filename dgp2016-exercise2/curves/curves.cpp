1205045888f1e77f870e4c8ac1092e54158290a0
agpmilli <alain.milliet@epfl.ch>
2016-10-05 13:45:51 +0200

    initial commit

--- dgp2016-exercise2/curves/curves.cpp
+++ dgp2016-exercise2/curves/curves.cpp
@@ -27,56 +27,10 @@ struct MainWindow : public TrackballWindow {
 
   void laplacianSmoothing() {
     // Curve Smoothing - centroid (this function should do one iteration of smoothing)
-    for (int i = 1; i < num_points-1; i++)
-    {
-      points(0, i) = (1-epsilon) * points(0, i) + (epsilon*((points(0, i-1)+points(0, i+1))/2));
-      points(1, i) = (1-epsilon) * points(1, i) + (epsilon*((points(1, i-1)+points(1, i+1))/2));
-    }
-
-    // Special for last and first points
-    points(0, 0) = (1-epsilon) * points(0, 0) + (epsilon*((points(0, num_points-1)+points(0, 1))/2));
-    points(1, 0) = (1-epsilon) * points(1, 0) + (epsilon*((points(1, num_points-1)+points(1, 1))/2));
-
-    points(0, num_points-1) = (1-epsilon) * points(0, num_points-1) + (epsilon*((points(0, num_points-2)+points(0, 1))/2));
-    points(1, num_points-1) = (1-epsilon) * points(1, num_points-1) + (epsilon*((points(1, num_points-2)+points(1, 1))/2));
-    // TODO uniformly scale the curve to its original length
   }
 
   void osculatingCircle() {
     // Curve Smoothing - osculating circle (again, this function should do one iteration of smoothing)
-      for (int i = 0; i < num_points; i++)
-      {
-        Vec2 point = Vec2(points(0,i), points(1,i));
-        double point0x, point0y, point1x, point1y, point2x, point2y;
-        point1x = points(0,i);
-        point1y = points(1,i);
-        if(i==num_points-1){
-            point2x = points(0,1);
-            point2y = points(1,1);
-        } else {
-            point2x = points(0,i+1);
-            point2y = points(1,i+1);
-        }
-        if(i==0){
-            point0x = points(0,num_points-1);
-            point0y = points(1,num_points-1);
-        } else {
-            point0x = points(0,i-1);
-            point0y = points(1,i-1);
-        }
-        // find center of the circumscribed circle
-        double D = 2 * ((point0x*(point1y-point2y)) + (point1x*(point2y-point0y)) + (point2x*(point0y-point1y)));
-        double Cx=(((pow(point0x,2) + pow(point0y,2))*(point1y - point2y)) + ((pow(point1x,2) + pow(point1y,2))*(point2y - point0y)) + ((pow(point2x,2) + pow(point2y,2))*(point0y - point1y))) / D;
-        double Cy=(((pow(point0x,2) + pow(point0y,2))*(point2x - point1x)) + ((pow(point1x,2) + pow(point1y,2))*(point0x - point2x)) + ((pow(point2x,2) + pow(point2y,2))*(point1x - point0x))) / D;
-        Vec2 C = Vec2(Cx, Cy);
-        Vec2 C_P = C-point;
-        double C_P_norm = sqrt(pow(C_P(0),2) + pow(C_P(1),2));
-        point = point + (epsilon * (C_P / pow(C_P_norm,2)));
-        points(0, i) = point(0);
-        points(1, i) = point(1);
-        // TODO tous les points sont pris en compte? Faire des tests
-      }
-      // TODO uniformly scale the curve to its original length
   }
 
 // ============================================================================
