4de76e3a24362c865ce7b67b83176f53caf7e907
agpmilli <alain.milliet@epfl.ch>
2016-10-07 13:06:28 +0200

    smooth/circle done

--- dgp2016-exercise2/curves/curves.cpp
+++ dgp2016-exercise2/curves/curves.cpp
@@ -23,37 +23,68 @@ struct MainWindow : public TrackballWindow {
 // Hint : try to play with epsilon
 // ============================================================================
   // time step for smoothing
-  double epsilon = 0.001;
+  double epsilon1 = 0.2;
+  double epsilon2 = 0.001;
 
   void laplacianSmoothing() {
     // Curve Smoothing - centroid (this function should do one iteration of smoothing)
+
+      // Original length
+      double L = 0;
+      // After transformation length
+      double L1 = 0;
+
     for (int i = 1; i < num_points-1; i++)
     {
-      points(0, i) = (1-epsilon) * points(0, i) + (epsilon*((points(0, i-1)+points(0, i+1))/2));
-      points(1, i) = (1-epsilon) * points(1, i) + (epsilon*((points(1, i-1)+points(1, i+1))/2));
+      // get size of each line
+      L+= sqrt(pow(points(0,i+1)-points(0,i),2) + pow(points(1,i+1)-points(1,i),2));
+      // Do the transformation for current point
+      points(0, i) = (1-epsilon1) * points(0, i) + (epsilon1*((points(0, i-1)+points(0, i+1))/2));
+      points(1, i) = (1-epsilon1) * points(1, i) + (epsilon1*((points(1, i-1)+points(1, i+1))/2));
+      // get size of each line after transformation
+      L1+= sqrt(pow(points(0,i+1)-points(0,i),2) + pow(points(1,i+1)-points(1,i),2));
     }
 
-    // Special for last and first points
-    points(0, 0) = (1-epsilon) * points(0, 0) + (epsilon*((points(0, num_points-1)+points(0, 1))/2));
-    points(1, 0) = (1-epsilon) * points(1, 0) + (epsilon*((points(1, num_points-1)+points(1, 1))/2));
+    // Special for last and first points (same as in the for loop)
+    L+= sqrt(pow(points(0,1)-points(0,1),2) + pow(points(1,0)-points(1,0),2));
+    points(0, 0) = (1-epsilon1) * points(0, 0) + (epsilon1*((points(0, num_points-1)+points(0, 1))/2));
+    points(1, 0) = (1-epsilon1) * points(1, 0) + (epsilon1*((points(1, num_points-1)+points(1, 1))/2));
+    L1+= sqrt(pow(points(0,1)-points(0,1),2) + pow(points(1,0)-points(1,0),2));
 
-    points(0, num_points-1) = (1-epsilon) * points(0, num_points-1) + (epsilon*((points(0, num_points-2)+points(0, 1))/2));
-    points(1, num_points-1) = (1-epsilon) * points(1, num_points-1) + (epsilon*((points(1, num_points-2)+points(1, 1))/2));
-    // TODO uniformly scale the curve to its original length
+    L+= sqrt(pow(points(0,num_points-1)-points(0,num_points-1),2) + pow(points(1,0)-points(1,0),2));
+    points(0, num_points-1) = (1-epsilon1) * points(0, num_points-1) + (epsilon1*((points(0, num_points-2)+points(0, 0))/2));
+    points(1, num_points-1) = (1-epsilon1) * points(1, num_points-1) + (epsilon1*((points(1, num_points-2)+points(1, 0))/2));
+    L1+= sqrt(pow(points(0,1)-points(0,1),2) + pow(points(1,0)-points(1,0),2));
+
+    // uniformly scale the curve back to its original length
+    points = points * (L/L1);
+
   }
 
   void osculatingCircle() {
     // Curve Smoothing - osculating circle (again, this function should do one iteration of smoothing)
+
+      // Original length
+      double L = 0;
+      // After transformation length
+      double L1 = 0;
+
       for (int i = 0; i < num_points; i++)
       {
+        // get the coordinates of current point
         Vec2 point = Vec2(points(0,i), points(1,i));
+        // create variables for coordinates of 3 points used to find the circumscribed circle
         double point0x, point0y, point1x, point1y, point2x, point2y;
         point1x = points(0,i);
         point1y = points(1,i);
         if(i==num_points-1){
-            point2x = points(0,1);
-            point2y = points(1,1);
+            // get size of each line
+            L+= sqrt(pow(points(0,0)-points(0,i),2) + pow(points(1,0)-points(1,i),2));
+            point2x = points(0,0);
+            point2y = points(1,0);
         } else {
+            // get size of each line
+            L+= sqrt(pow(points(0,i+1)-points(0,i),2) + pow(points(1,i+1)-points(1,i),2));
             point2x = points(0,i+1);
             point2y = points(1,i+1);
         }
@@ -64,6 +95,7 @@ struct MainWindow : public TrackballWindow {
             point0x = points(0,i-1);
             point0y = points(1,i-1);
         }
+
         // find center of the circumscribed circle
         double D = 2 * ((point0x*(point1y-point2y)) + (point1x*(point2y-point0y)) + (point2x*(point0y-point1y)));
         double Cx=(((pow(point0x,2) + pow(point0y,2))*(point1y - point2y)) + ((pow(point1x,2) + pow(point1y,2))*(point2y - point0y)) + ((pow(point2x,2) + pow(point2y,2))*(point0y - point1y))) / D;
@@ -71,12 +103,21 @@ struct MainWindow : public TrackballWindow {
         Vec2 C = Vec2(Cx, Cy);
         Vec2 C_P = C-point;
         double C_P_norm = sqrt(pow(C_P(0),2) + pow(C_P(1),2));
-        point = point + (epsilon * (C_P / pow(C_P_norm,2)));
+        point = point + (epsilon2 * (C_P / pow(C_P_norm,2)));
         points(0, i) = point(0);
         points(1, i) = point(1);
-        // TODO tous les points sont pris en compte? Faire des tests
+
+        if(i==num_points-1){
+            // get size of each line after transformation
+            L1+= sqrt(pow(points(0,0)-points(0,i),2) + pow(points(1,0)-points(1,i),2));
+        } else {
+            // get size of each line after transformation
+            L1+= sqrt(pow(points(0,i+1)-points(0,i),2) + pow(points(1,i+1)-points(1,i),2));
+        }
+
       }
-      // TODO uniformly scale the curve to its original length
+      // uniformly scale the curve back to its original length
+      points = points * (L/L1);
   }
 
 // ============================================================================
