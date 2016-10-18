87a3df65c6eb856447ad0eb8e4516ca4c6b289e6
rbsteinm <raphael.steinmann@epfl.ch>
2016-10-18 14:57:18 +0200

    remove unused and misplaced build files

--- Ex3/nanogui/ext_build/glfw/src/CMakeFiles/glfw.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "glfw3.pdb"
-  "glfw3.lib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/glfw.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
